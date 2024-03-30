147c9f209cdc450b55660f65dbd7d14948b54b4a
mohsensami <mohsensami1367@gmail.com>
2024-03-30 21:59:48 +0330

    Add meeting event, forms and dates

---
+++ meeting-scheduler/app/(routes)/dashboard/meeting-type/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MettingType = () => {
+    return <div>MettingType</div>;
+};
+
+export default MettingType;
--- meeting-scheduler/app/(routes)/dashboard/page.jsx
+++ meeting-scheduler/app/(routes)/dashboard/page.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { app } from '@/config/FirebaseConfig';
 import { useRouter } from 'next/navigation';
-// import MeetingType from './meeting-type/page';
+import MeetingType from './meeting-type/page';
 function Dashboard() {
     const db = getFirestore(app);
     const { user } = useKindeBrowserClient();
@@ -34,7 +34,11 @@ function Dashboard() {
         return <h2>Loading...</h2>;
     }
 
-    return <div>{/* <MeetingType /> */}</div>;
+    return (
+        <div>
+            <MeetingType />
+        </div>
+    );
 }
 
 export default Dashboard;
